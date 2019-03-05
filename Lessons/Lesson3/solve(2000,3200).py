13ea60b1e070db1f87e6970bde661258b9a9e32f
ovr1 <rybynok@mail.ru>
2019-03-05 17:52:58 +0300

    Web

--- Lessons/Lesson3/solve(2000,3200).py
+++ Lessons/Lesson3/solve(2000,3200).py
@@ -1,9 +1,9 @@
 solution = []
-def solv():
-    for i in range(2000,3200):
+def solv(num1, num2):
+    for i in range(num1, num2):
         if i % 7 == 0 and i % 5 != 0:
             solution.append(str(i))
             continue
     return solution
-solv()
+solv(2000,3200)
 print('{0}'.format(solution))

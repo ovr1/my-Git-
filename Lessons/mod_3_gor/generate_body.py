28b749b011953002e9f67f31bca72e52f1832eff
ovr1 <rybynok@mail.ru>
2019-03-05 11:54:41 +0300

    Web

--- Lessons/mod_3_gor/generate_body.py
+++ Lessons/mod_3_gor/generate_body.py
@@ -3,3 +3,18 @@ def generate_body(header, paragraphs):
     for p in paragraphs:
         body = body + f"<p>{p}</p>"
     return f"<body>{body}</body>"
+
+#def generate_body(header, paragraphs):
+    #body = "<h1>" + header + "</h1>"
+    #for p in paragraphs:
+        #body = body + "<p>" + p + "</p>"
+    #return "<body> + body + </body>"
+
+#def generate_body(header, paragraphs):
+    #body = "<h1>" + header + "</h1>"
+    #i = 0
+    #while i < len(paragraphs):
+        #body = body + "<p>" + paragraphs[i] + "</p>"
+        #i = i  + 1
+    #return "<body>" + body +</body>"
+

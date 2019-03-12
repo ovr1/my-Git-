8571c65366ea9570bea546db0294b7f0452cb3fe
ovr1 <rybynok@mail.ru>
2019-03-12 19:21:38 +0300

    Web

--- Lessons/Lesson3/mod_3_gor/server/server.py
+++ Lessons/Lesson3/mod_3_gor/server/server.py
@@ -1,9 +1,10 @@
 from bottle import route, run, view
-
+from datetime import datetime as dt
 @route('/')
-view ("predictions")
+@view ("predictions")
 def index():
-    return {"date":}
+    now = dt.now()
+    return {"date": f"{now.year}-{now.month}-{now.day}"}
 
 @route("/api/test")
 def api_test():

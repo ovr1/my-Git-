698b854f74e01a1e6f8fd8db0fa1571aa1f4303d
ovr1 <rybynok@mail.ru>
2019-03-13 19:40:17 +0300

    Web

--- Lessons/Lesson4/server2/server2.py
+++ Lessons/Lesson4/server2/server2.py
@@ -1,24 +1,20 @@
 from bottle import route, run, view
 from datetime import datetime as dt
 from random import random
+from horoscope import generate_predictions
 
 @route("/")
-@view("predictions")
+@view("predictions2")
 def index():
   now = dt.now()
-
+  predictions = generate_predictions()
   x = random()
 
   return {
     "date": f"{now.year}-{now.month}-{now.day}",
-    "predictions": [
-      "После обеда ожидайте неожиданного праздника.",
-      "Днем остерегайтесь неожиданного праздника.",
-      "Утром ожидайте гостей из забытого прошлого.",
-    ],
-    "special_date": x > 0.5,
-    "x": x,
+    "predictions": f"{generate_predictions()}",
   }
+()
 
 @route("/api/test")
 def api_test():
---
+++ Lessons/Lesson4/server2/views/horoscope.py
@@ -0,0 +1,33 @@
+import random
+
+times = ["утром", "днем", "вечером", "ночью", "после обеда", "перед сном"]
+advices = ["ожидайте", "предостерегайтесь", "будьте открыты для"]
+promises = ["гостей из забытого прошлого", "встреч со старыми знакомыми",
+            "неожиданного праздника", "приятных перемен"]
+
+
+def generate_predictions(total_num=5, num_sentences=3):
+    predictions = []
+
+    for i in range(total_num):
+        forecast = ""
+        for j in range(num_sentences):
+            t = random.choice(times)
+            a = random.choice(advices)
+            p = random.choice(promises)
+
+            full_sentence = f"{t.title()} {a} {p}."
+            if j != num_sentences - 1:
+                full_sentence = full_sentence + " "
+
+            forecast = forecast + full_sentence
+
+        predictions.append(forecast)
+
+    return predictions
+
+
+
+
+
+

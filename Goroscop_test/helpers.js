fc8c28807f726c38f41b91e4694a53581856d20d
ovr1 <rybynok@mail.ru>
2019-03-14 14:02:07 +0300

    Configuration

--- Goroscop_test/helpers.js
+++
@@ -1,9 +0,0 @@
-$("#kalendar").click(function() {
-	consol.log("1. Внутри анонимной функци")
-	p = $("#pred1");
-	p.html("Privet")
-	p.css("color", "red")
-	
-	console.log("2 :после вызова стайлинга")
-})
-	
---
+++ Goroscop_test/views/helpers.js
@@ -0,0 +1,5 @@
+$("#kalendar").click(function() {
+	console.log("1. Внутри анонимной функци")
+	console.log("2 :после вызова стайлинга")
+})
+	

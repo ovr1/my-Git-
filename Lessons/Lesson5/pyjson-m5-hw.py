79cc09fa6c2bfa4d0da78ea67c274118d002a325
ovr1 <rybynok@mail.ru>
2019-03-20 00:38:57 +0300

    Web

--- Lessons/Lesson5/pyjson-m5-hw.py
+++ Lessons/Lesson5/pyjson-m5-hw.py
@@ -10,19 +10,22 @@ with open("m5-access-log-100.json") as fp:
 #print(data)
 
     for record in data:
-        l = record["ip"], record["timestamp"]
+        l = record["ip"], record["timestamp"],record["user-agent"]
 
     for record in data:
         if record["ip"] in freq_dict:
             freq_dict[record["ip"]] = freq_dict[record["ip"]] + 1
+            for ip in freq_dict:
+                f = freq_dict[ip]
+                if f == 54:
+                    print(f, record["ip"], record["timestamp"], record["user-agent"])
+                else:
+                    print(f, ip)
         else:
             freq_dict[record["ip"]] = 1
+            print(record["ip"])
 
-    for ip in freq_dict:
-        f = freq_dict[ip]
-        if f == 54:
-            print(ip)
 
-    for ip in freq_dict:
-        f = freq_dict[ip]
-        print(ip, f)
+
+
+
--- Lessons/Lesson5/pypandas-m5-hw2.py
+++ Lessons/Lesson5/pypandas-m5-hw2.py
@@ -1,25 +1,14 @@
 import pandas
-import json
 
 freq_dict = {}
 data = pandas.read_json("m5-access-log-100.2.json")
-print(data["ip"].value_counts())
+print("ip: ",data["ip"].max())
+print("user-agent: ",data["user-agent"].max())
+print("timestamp: ",data["timestamp"].max())
+print(data["ip"].value_counts().max())
+print(len(data))
 
-with open("m5-access-log-100.2.json") as fp:
-    data = json.load(fp)
-    print(len(data))
 
-    for record in data:
-        print(record["ip"], record["timestamp"],record["user-agent"])
-        #print(record["ip"], record["timestamp"],record["user-agent"])
-    for record in data:
-        if record["ip"] in freq_dict:
-            freq_dict[record["ip"]] = freq_dict[record["ip"]] + 1
-        else:
-            freq_dict[record["ip"]] = 1
-    for ip in freq_dict:
-        f = freq_dict[ip]
-        print(f)
-        if f == 54:
-            print(ip)
+
+
 

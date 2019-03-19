b12ee4e3d5ce9cf2d225396d4697916755c6e40c
ovr1 <rybynok@mail.ru>
2019-03-19 15:30:08 +0300

    Web

--- Lessons/Lesson5/pypandas-m5-hw2.py
+++ Lessons/Lesson5/pypandas-m5-hw2.py
@@ -1,28 +1,25 @@
+import pandas
 import json
 
-from numpy.distutils.fcompiler import none
-
 freq_dict = {}
-N =[]
+data = pandas.read_json("m5-access-log-100.2.json")
+print(data["ip"].value_counts())
 
-with open("m5-access-log-100.json") as fp:
+with open("m5-access-log-100.2.json") as fp:
     data = json.load(fp)
-#print(data)
+    print(len(data))
 
     for record in data:
-        l = record["ip"], record["timestamp"]
-
+        print(record["ip"], record["timestamp"],record["user-agent"])
+        #print(record["ip"], record["timestamp"],record["user-agent"])
     for record in data:
         if record["ip"] in freq_dict:
             freq_dict[record["ip"]] = freq_dict[record["ip"]] + 1
         else:
             freq_dict[record["ip"]] = 1
-
     for ip in freq_dict:
         f = freq_dict[ip]
+        print(f)
         if f == 54:
             print(ip)
 
-    for ip in freq_dict:
-        f = freq_dict[ip]
-        print(ip, f)

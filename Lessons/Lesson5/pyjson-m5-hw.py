0a06c7e04b8db4f2499c381fdbc22ec3d399a29e
ovr1 <rybynok@mail.ru>
2019-03-19 10:35:36 +0300

    Web

---
+++ Lessons/Lesson5/pyjson-m5-hw.py
@@ -0,0 +1,24 @@
+import json
+
+from numpy.distutils.fcompiler import none
+
+freq_dict = {}
+
+with open("m5-access-log-100.json") as fp:
+    data = json.load(fp)
+#print(data)
+
+    #for record in data:
+        #print(record["ip"], record["timestamp"])
+
+
+    for record in data:
+        if record["ip"] in freq_dict:
+            freq_dict[record["ip"]] = freq_dict[record["ip"]] + 1
+        else:
+            freq_dict[record["ip"]] = 1
+
+    for ip in freq_dict:
+        f = freq_dict[ip]
+        if f >=3:
+            print(f, ip)

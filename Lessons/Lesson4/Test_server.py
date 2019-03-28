00e0810aa19d276f12b98c1db5ba43ce97925c2d
ovr1 <rybynok@mail.ru>
2019-03-28 11:17:17 +0300

    Web

--- Lessons/Lesson4/Test_server.py
+++ Lessons/Lesson4/Test_server.py
@@ -13,5 +13,16 @@ def  show_wiki_page ( pagename ):
 #@route ( '/ <action> / <пользователь>' )             # соответствует / follow / defnull
 #def  user_api (action ,  пользователь):
 
+#@route('/object/<id:int>')
+#def callback(id):
+#    assert isinstance(id, int)
+
+#@route('/show/<name:re:[a-z]+>')
+#def callback(name):
+#   assert name.isalpha()
+
+#@route('/static/<path:path>')
+#def callback(path):
+#   return static_file(path, ...)
 
 run(host='localhost', port=8080, debug=True)

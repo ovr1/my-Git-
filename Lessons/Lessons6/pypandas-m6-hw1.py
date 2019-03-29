e40738b1f0af2fa570f63176788da3de39e97566
ovr1 <rybynok@mail.ru>
2019-03-30 00:50:10 +0300

    Web

---
+++ Lessons/Lessons6/pypandas-m6-hw1.py
@@ -0,0 +1,39 @@
+from datetime import datetime as dt
+import sqlite3 as lite
+import sys
+
+S = []
+L = []
+N = []
+
+con = lite.connect('access_log.db')
+
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM main.logs[access_log]")
+    rows = cur.fetchall()
+    for row in rows[1:]:
+        if "109.234.35.42"  in row:
+            S.append(row)
+
+print("S = ",len(S))
+print("N = ", len(rows))
+
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM main.logs[access_log] WHERE TIMESTAMP LIKE '2018-09-01%'")
+    rows = cur.fetchall()
+    for row in rows[1:]:
+        L.append(row)
+
+l = print("L = ",len(L))
+
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM main.logs[access_log] WHERE TIMESTAMP LIKE '2018-09-10%'")
+    rows = cur.fetchall()
+    for row in rows[1:]:
+        N.append(row)
+
+n = print("N = ",len(N))
+

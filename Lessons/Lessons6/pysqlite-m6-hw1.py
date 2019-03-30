5333e4979f330f7816fe76ba56ee545413566baa
ovr1 <rybynok@mail.ru>
2019-03-31 00:57:40 +0300

    Configuration
    Signed-off-by: ovr1 <rybynok@mail.ru>

--- Lessons/Lessons6/pysqlite-m6-hw1.py
+++ Lessons/Lessons6/pysqlite-m6-hw1.py
@@ -21,10 +21,27 @@ print("N = ", len(rows))
 
 with con:
     cur = con.cursor()
-    cur.execute("SELECT * FROM main.logs[access_log] WHERE TIMESTAMP > LIKE '2018-09-01%'AND TIMESTAMP < LIKE '2018-09-10%'")
+    cur.execute("SELECT * FROM main.logs[access_log] WHERE TIMESTAMP > '2018-09-01 00:00:00.000000'AND TIMESTAMP < '2018-09-10 00:00:00.000000'")
     rows = cur.fetchall()
-    for row in rows[1:]:
+    for row in rows:
         L.append(row)
 print("L = ",len(L))
 
+fd = {}
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM main.logs[access_log]")
+    rows = cur.fetchall()
+    for row in rows:
+        ip = row[1]
+        if ip in fd:
+            n = fd[ip]
+            fd[ip] = fd[ip] + 1
+        else:
+            fd[ip] = 1
+for k,v in fd.items():
+    if int(v) > 2850:
+        print(v,k)
 
+
+l = (73875 + 17970 + 9878)/710424*100

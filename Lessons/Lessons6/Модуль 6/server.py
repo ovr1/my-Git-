b4370689c34b30297e53fe417943c7c290861a84
ovr1 <rybynok@mail.ru>
2019-03-31 14:42:59 +0300

    Web

--- Lessons/Lessons6/Модуль 6/server.py
+++ Lessons/Lessons6/Модуль 6/server.py
@@ -2,38 +2,23 @@ from bottle import route, run, static_file, view, redirect, request
 from db import TodoItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
 
 engine = create_engine("sqlite:///tasks.db")
 Session = sessionmaker(bind=engine)
 s = Session()
 
-#class TodoItem:
-#   def __init__(self, description, unique_id):
-#       self.description = description
-#       self.is_completed = False
-#       self.uid = unique_id
 
-#   def __str__(self):
-#       return self.description.lower()
-
-#tasks_db = {
-#   1: TodoItem("прочитать книгу",1),
-#   2: TodoItem("учиться жонглировать 30 минут", 2),
-#   3: TodoItem("помыть посуду", 3),
-#   4: TodoItem("поесть", 4),
-#}
- 
-@route("/static/<filename>")
+@route("/static/<filename:path>")
 def send_static(filename):
     return static_file(filename, root="static")
 
-cwd = os.getcwd() + os.sep + "view" + os.sep + "index.tpl"
+
 @route("/")
-@view(cwd)
+@view("index")
 def index():
     tasks = s.query(TodoItem).order_by(TodoItem.uid)
     return {"tasks": tasks}
+
 
 @route("/add-task", method="POST")
 def add_task():
@@ -51,13 +36,13 @@ def api_delete(uid):
     s.commit()
     return redirect("/")
 
+
 @route("/api/complete/<uid:int>")
 def api_complete(uid):
-    t = s.query(TodoItem).filter(TodoItem.uid == uid).delete()
+    t = s.query(TodoItem).filter(TodoItem.uid == uid).first()
     t.is_completed = True
     s.commit()
     return "Ok"
 
-
 ###
-run(host="localhost", port=8085, autoreload=True)
+run(host="localhost", port=8080)
